39d4b197d4abc1c370ee36d0ed53bc9986b0f12e
SalihTasdelen <salih.tasdelen@hotmail.com>
2023-04-17 04:35:05 +0300

    added residual class

---
+++ src/remin/residual.py
@@ -0,0 +1,18 @@
+import torch
+import numpy as np
+
+class Residual:
+
+    def __init__(self, domain, equation):
+        if type(domain) == np.ndarray:
+            domain = torch.from_numpy(domain).type(torch.float32)
+        self.domain = domain
+        self.xs = self.split_domain(domain)
+        self.equation = equation
+
+    @staticmethod
+    def split_domain(domain: torch.tensor):
+        xs = torch.hsplit(domain, domain.shape[1])
+        for x in xs:
+            x.requires_grad_()
+        return xs
