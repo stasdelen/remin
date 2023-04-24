ac9140156ba3d7889f90302774fa0fe28a74b834
SalihTasdelen <salih.tasdelen@hotmail.com>
2023-04-24 23:54:59 +0300

    format files

--- src/remin/residual.py
+++ src/remin/residual.py
@@ -1,31 +1,27 @@
 import torch
-from torch.utils.data import (
-    Dataset,
-    ConcatDataset,
-    BatchSampler,
-    DataLoader,
-    default_collate
-)
+from torch.utils.data import (Dataset, ConcatDataset, BatchSampler, DataLoader)
+
 
 class Residual:
 
-    def __init__(self, domain, equations, batch_size = None):
+    def __init__(self, domain, equations, batch_size=None):
         self.domain = domain
         if isinstance(domain, (list, tuple)):
             self.equations = equations
         self.equations = [equations]
         if batch_size is None: self.batch_size = len(domain)
         else: self.batch_size = batch_size
-    
+
     def to_torch(self, device):
         return torch.from_numpy(self.domain).float().to(device)
-    
+
     @staticmethod
     def split_domain(domain):
         xs = torch.hsplit(domain, domain.shape[1])
         for x in xs:
             x.requires_grad_()
         return xs
+
 
 class ResidualDataset(Dataset):
 
@@ -37,16 +33,17 @@ class ResidualDataset(Dataset):
 
     def __getitem__(self, index):
         return self.domain[index]
-    
+
+
 class ResBatchSampler(BatchSampler):
 
     def __init__(self, cumulative_sizes, batch_sizes) -> None:
         self.cumul_sizes = cumulative_sizes
         self.batch_sizes = batch_sizes
-        self.batch_size = sum(batch_sizes) 
+        self.batch_size = sum(batch_sizes)
         self.ndata = len(batch_sizes)
 
-    def __iter__(self):    
+    def __iter__(self):
         for i in range(self.cumul_sizes[-1] // self.batch_size):
             batch = []
             start_idx = 0
@@ -57,6 +54,7 @@ class ResBatchSampler(BatchSampler):
                 batch += list(range(start, start + batch_size))
                 start_idx = cumul_size
             yield batch
+
 
 class ResidualLoader:
 
@@ -84,25 +82,21 @@ class ResidualLoader:
         for i in range(self.numres):
             self.functions[i] = self.residuals[i].equations
             self.xs[i] = self.residuals[i].to_torch(device)
-    
+
     def instanciate_data_loader(self):
         for i in range(self.numres):
             self.batch_sizes[i] = self.residuals[i].batch_size
             self.functions[i] = self.residuals[i].equations
             self.datasets[i] = ResidualDataset(self.residuals[i].domain)
-        
+
         self.dataset = ConcatDataset(self.datasets)
         self.vsplit = [self.batch_sizes[0]]
         for batch_size in self.batch_sizes[1:-1]:
             self.vsplit.append(self.vsplit[-1] + batch_size)
         if self.batched:
-            self.batch_sampler = ResBatchSampler(
-                self.dataset.cumulative_sizes,
-                self.batch_sizes
-            )
-        self.loader = DataLoader(
-            self.dataset,
-            batch_size=len(self.dataset),
-            batch_sampler=self.batch_sampler,
-            **self.args
-        )
+            self.batch_sampler = ResBatchSampler(self.dataset.cumulative_sizes,
+                                                 self.batch_sizes)
+        self.loader = DataLoader(self.dataset,
+                                 batch_size=len(self.dataset),
+                                 batch_sampler=self.batch_sampler,
+                                 **self.args)
--- src/remin/solver.py
+++ src/remin/solver.py
@@ -3,14 +3,16 @@ from .func import traceable
 from .residual import ResidualLoader, Residual
 from time import thread_time
 
+
 class Solver:
 
-    def __init__(self, name, model, residual_loader: ResidualLoader, optimizer, lossfunc) -> None:
+    def __init__(self, name, model, residual_loader: ResidualLoader, optimizer,
+                 lossfunc) -> None:
         self.name = name
         self.model = model
         self.device = next(model.parameters()).device
         self.params = dict(model.named_parameters())
-        
+
         self.residual_loader = residual_loader
         self.residual_loader.instanciate(self.device)
         if self.residual_loader.fully_loaded:
@@ -30,24 +32,25 @@ class Solver:
         self.lossfunc = state_dict['loss']
 
     def save(self, epoch, name):
-            torch.save({
-                'epoch': epoch+1,
+        torch.save(
+            {
+                'epoch': epoch + 1,
                 'model_state_dict': self.model.state_dict(),
                 'optimizer_state_dict': self.optimizer.state_dict(),
                 'loss': self.lossfunc,
-                }, name)
+            }, name)
 
     def compile(self, backend='inductor', fullgraph=False):
         if self.lossfunc is None:
             raise ValueError('Loss function must be defined.')
-        
+
         def _residual_loss(params, xs):
             resloss = 0
             for i in range(self.residual_loader.numres):
                 domain = Residual.split_domain(xs[i])
                 for function in self.residual_loader.functions[i]:
                     resloss += self.lossfunc(function(params, *domain),
-                                    torch.zeros_like(domain[0]))
+                                             torch.zeros_like(domain[0]))
             return resloss
 
         residual_loss = torch.compile(traceable(_residual_loss),
@@ -59,10 +62,8 @@ class Solver:
     def _train_loop_dataloader(self):
         resloss = 0
         for i, data in enumerate(self.residual_loader.loader):
-            xs = torch.vsplit(
-                data.to(self.device, non_blocking=True),
-                self.residual_loader.vsplit
-            )
+            xs = torch.vsplit(data.to(self.device, non_blocking=True),
+                              self.residual_loader.vsplit)
             resloss += self.residual_loss(self.params, xs)
         resloss /= i + 1.0
         # Backprop
@@ -71,7 +72,7 @@ class Solver:
         self.optimizer.step()
 
         return resloss
-    
+
     def _train_loop_fullloaded(self):
         resloss = self.residual_loss(self.params, self.residual_loader.xs)
 
@@ -81,7 +82,6 @@ class Solver:
         self.optimizer.step()
 
         return resloss
-
 
     def fit(self, epochs=1, log_epoch=100, log_progress=10):
         if self.residual_loss is None:
@@ -94,26 +94,30 @@ class Solver:
         for epoch in range(epochs):
             t0 = thread_time()
             resloss = self.train_loop()
-            t_ave = (t_ave*epoch + (thread_time() - t0))/(epoch + 1)
+            t_ave = (t_ave * epoch + (thread_time() - t0)) / (epoch + 1)
             if epoch % log_progress == 0:
-                _printProgress(
-                    epoch,
-                    epochs,
-                    log_epoch,
-                    f'Loss: {resloss:10.6f}',
-                    f'{(t_ave*1e3):7.3f}ms/epoch'
-                )
-            
+                _printProgress(epoch, epochs, log_epoch, f'Loss: {resloss:10.6f}',
+                               f'{(t_ave*1e3):7.3f}ms/epoch')
+
             if resloss < best_loss:
                 best_loss = resloss
                 self.save(epoch, self.name + '_best_model.pt')
         self.save(epoch, self.name + '_final_model.pt')
 
-def _printProgress(epoch, epochs, log_epoch, prefix = '', suffix = '', length = 20, fill = '#', printEnd = '\r'):
+
+def _printProgress(epoch,
+                   epochs,
+                   log_epoch,
+                   prefix='',
+                   suffix='',
+                   length=20,
+                   fill='#',
+                   printEnd='\r'):
     filledLength = int(length * (epoch % log_epoch) // log_epoch)
-    if epoch % log_epoch == 0: 
+    if epoch % log_epoch == 0:
         filledLength = length
         printEnd = '\n'
     bar = fill * filledLength + '.' * (length - filledLength)
-    print(f'{prefix} [{bar}] [{epoch:>5d}/{epochs:>5d}]% {suffix}', flush=True, end = printEnd)
-    
+    print(f'{prefix} [{bar}] [{epoch:>5d}/{epochs:>5d}]% {suffix}',
+          flush=True,
+          end=printEnd)
