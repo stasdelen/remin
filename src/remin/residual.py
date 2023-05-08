92ba8959eef4e463adaf5b48f387d1b9d6448664
SalihTasdelen <salih.tasdelen@hotmail.com>
2023-05-09 00:04:23 +0300

    fixed batching algo

--- src/remin/residual.py
+++ src/remin/residual.py
@@ -4,8 +4,9 @@ from torch.utils.data import (Dataset, ConcatDataset, BatchSampler, DataLoader)
 
 class Residual:
 
-    def __init__(self, domain, equations, batch_size=None):
+    def __init__(self, domain, equations, weight=1.0, batch_size=None):
         self.domain = domain
+        self.weight = weight
         if isinstance(equations, (list, tuple)):
             self.equations = equations
         else:
@@ -14,7 +15,7 @@ class Residual:
         else: self.batch_size = batch_size
 
     def to_torch(self, device):
-        return torch.from_numpy(self.domain).float().to(device)
+        return torch.from_numpy(self.domain.astype('float32')).to(device)
 
     @staticmethod
     def split_domain(domain):
@@ -27,7 +28,7 @@ class Residual:
 class ResidualDataset(Dataset):
 
     def __init__(self, domain) -> None:
-        self.domain = torch.from_numpy(domain).float()
+        self.domain = torch.from_numpy(domain.astype('float32'))
 
     def __len__(self):
         return len(self.domain)
@@ -43,61 +44,80 @@ class ResBatchSampler(BatchSampler):
         self.batch_sizes = batch_sizes
         self.batch_size = sum(batch_sizes)
         self.ndata = len(batch_sizes)
+        self.n_batches = self.cumul_sizes[0] // self.batch_sizes[0]
+        for i in range(1, self.ndata):
+            n = (self.cumul_sizes[i] - self.cumul_sizes[i - 1]) // self.batch_sizes[i]
+            if n > self.n_batches: self.n_batches = n
+
         self.batch = [None] * self.batch_size
         self.batch_shift = [None] * self.batch_size
         start_idx, batch_idx = 0, 0
         for i in range(self.ndata):
             batch_size = self.batch_sizes[i]
             cumul_size = self.cumul_sizes[i]
+            prev_cumul_size = self.cumul_sizes[i - 1] if i > 0 else 0
             for j in range(batch_size):
-                self.batch_shift[batch_idx + j] = (batch_size, cumul_size)
+                self.batch_shift[batch_idx + j] = (batch_size, cumul_size,
+                                                   prev_cumul_size)
                 self.batch[batch_idx + j] = start_idx + j
             start_idx = cumul_size
             batch_idx += batch_size
 
     def __iter__(self):
-        for i in range(self.cumul_sizes[-1] // self.batch_size):
-            for j in range(self.batch_size):
-                batch_size, cumul_size = self.batch_shift[j]
-                self.batch[j] = (self.batch[j] + batch_size * i) % cumul_size
+        for _ in range(self.n_batches):
             yield self.batch
+            for j in range(self.batch_size):
+                batch_size, cumul_size, prev_cumul_size = self.batch_shift[j]
+                self.batch[j] = (self.batch[j] + batch_size)
+                if self.batch[j] >= cumul_size:
+                    self.batch[j] -= cumul_size - prev_cumul_size
 
 
-class ResidualLoader:
+class Loader:
 
-    def __init__(self, residuals, fully_loaded=False, batched=False, **args) -> None:
-        if fully_loaded and batched:
-            raise ValueError('Fully loaded models can not be batched.')
+    def __init__(self, residuals, **kwargs) -> None:
         self.residuals = residuals
-        self.fully_loaded = fully_loaded
-        self.batched = batched
-        self.numres = len(residuals)
-        self.functions = [None] * self.numres
-        self.xs = [None] * self.numres
-        self.batch_sizes = [None] * self.numres
-        self.datasets = [None] * self.numres
-        self.batch_sampler = None
-        self.args = args
+        self.n_res = len(residuals)
+        self.functions = [None] * self.n_res
+        self.weights = [None] * self.n_res
+        self.kwargs = kwargs
+
+    def instanciate(self):
+        raise NotImplementedError('Base class cannot be instanciated.')
+
+
+class FullLoader(Loader):
+
+    def __init__(self, residuals, **kwargs) -> None:
+        super().__init__(residuals, **kwargs)
+        self.xs = [None] * self.n_res
 
     def instanciate(self, device):
-        if self.fully_loaded:
-            self.instanciate_fully_loaded(device)
-        else:
-            self.instanciate_data_loader()
-
-    def instanciate_fully_loaded(self, device):
-        for i in range(self.numres):
+        for i in range(self.n_res):
             self.functions[i] = self.residuals[i].equations
             self.xs[i] = self.residuals[i].to_torch(device)
+            self.weights[i] = self.residuals[i].weight
 
-    def instanciate_data_loader(self):
-        for i in range(self.numres):
+
+class BatchLoader(Loader):
+
+    def __init__(self, residuals, batched, **kwargs) -> None:
+        super().__init__(residuals, **kwargs)
+        self.batch_sizes = [None] * self.n_res
+        self.datasets = [None] * self.n_res
+        self.batch_sampler = None
+        self.batched = batched
+
+        for i in range(self.n_res):
             self.batch_sizes[i] = self.residuals[i].batch_size
             self.functions[i] = self.residuals[i].equations
+            self.weights[i] = self.residuals[i].weight
             self.datasets[i] = ResidualDataset(self.residuals[i].domain)
 
         self.dataset = ConcatDataset(self.datasets)
+        self.dataset_size = len(self.dataset)
         self.vsplit = [self.batch_sizes[0]]
+
         for batch_size in self.batch_sizes[1:-1]:
             self.vsplit.append(self.vsplit[-1] + batch_size)
         if self.batched:
@@ -105,8 +125,17 @@ class ResidualLoader:
                                                  self.batch_sizes)
             self.loader = DataLoader(self.dataset,
                                      batch_sampler=self.batch_sampler,
-                                     **self.args)
+                                     **self.kwargs)
         else:
             self.loader = DataLoader(self.dataset,
-                                     batch_size=len(self.dataset),
-                                     **self.args)
+                                     batch_size=self.dataset_size,
+                                     **self.kwargs)
+
+
+def make_loader(residuals, fully_loaded=False, batched=False, **kwargs):
+    if fully_loaded and batched:
+        raise ValueError('Data can not be fully loaded and batched at the same time.')
+    if fully_loaded:
+        return FullLoader(residuals, **kwargs)
+    else:
+        return BatchLoader(residuals, batched, **kwargs)
