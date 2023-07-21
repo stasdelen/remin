51995edb19649309e67e1bc4c5ba546474de1d1f
SalihTasdelen <salih.tasdelen@hotmail.com>
2023-07-21 13:33:37 +0300

    fix postprocess

--- examples/ldc/ldc_post.py
+++ examples/ldc/ldc_post.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 	file_name = args.file
 	if args.file is None:
-		file_name = 'LDC_Re100_uvp/LDC_Re100_best.pt'
-		# file_name = 'LDC_Re100_uvp/LDC_Re100_final.pt'
+		file_name = 'outputs/LDC_Re100_best.pt'
+		# file_name = 'outputs/LDC_Re100_final.pt'
 
 	model = Ldc()
 	mdata = torch.load(file_name)
 	model.load_state_dict(mdata['model_state_dict'])
 	model.eval()
 
-	x = torch.linspace(0,1,100)
-	y = torch.linspace(0,1,100)
+	x = torch.linspace(0,1,1000)
+	y = torch.linspace(0,1,1000)
 
 	X, Y = torch.meshgrid(x, y, indexing='xy')
 	X = X.flatten()[:,None].requires_grad_()
@@ -39,3 +39,4 @@ if __name__ == '__main__':
 	
 	plt.colorbar()
 	plt.show()
+	plt.savefig(file_name.split('.')[0] + '.png', dpi=300)
